126950ef216f1c715df3a85f1e9d48a78b0ed241
Ethan Brouwer <eabrouwer3@gmail.com>
2021-02-25 09:26:33 -0700

    Updated config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,4 @@ pyenv init - | source
 set -x PIPENV_PYTHON $PYENV_ROOT/shims/python
 
 # TaxBit Tunnels
-alias dbtunnel='ssh -L 5432:staging-cluster.cluster-coj8zyvaav5h.us-east-1.rds.amazonaws.com:5432 ubuntu@staging-server.taxbit.com -i ~/.ssh/taxbit'
-alias assettunnel='ssh -L 8888:buxwwn6cel.execute-api.us-east-1.amazonaws.com:443 ubuntu@staging-server.taxbit.com -i ~/.ssh/taxbit'
 alias jup='pipenv run jupyter notebook'
